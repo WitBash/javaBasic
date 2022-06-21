162c9ce8337fdb44ddf817c163c4f003c3b5f99a
WitBash <yr-83p@mail.ru>
2022-06-21 21:36:53 +0300

    fix hw2

--- src/com/boshko/lesson2/hw2/Flat.java
+++ src/com/boshko/lesson2/hw2/Flat.java
@@ -7,14 +7,58 @@ public class Flat {
     private int floor;
     private long price;
 
-    public Flat() {
-
-    }
 
     public Flat(double square, String address, int floor, long price) {
         this.square = square;
         this.address = address;
         this.floor = floor;
         this.price = price;
+    }
+
+    public double getSquare() {
+        return square;
+    }
+
+    public void setSquare(double square) {
+        this.square = square;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Flat{" +
+                "square=" + square +
+                ", address='" + address + '\'' +
+                ", floor=" + floor +
+                ", price=" + price +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Flat flat = new Flat(80.5,"Санкт-Петербург",8, 16000000);
+        System.out.println(flat);
     }
 }
