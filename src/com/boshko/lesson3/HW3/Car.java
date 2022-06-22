5bd2c44408d771fd96156bf5b18c4cd1fa79563f
WitBash <yr-83p@mail.ru>
2022-06-23 00:05:03 +0300

    added method main to class Car

--- src/com/boshko/lesson3/HW3/Car.java
+++ src/com/boshko/lesson3/HW3/Car.java
@@ -7,7 +7,7 @@ public class Car {
     private String titleModel;
     private int weight;
 
-    public Car (Color color){
+    public Car(Color color) {
         this.color = color;
     }
 
@@ -16,7 +16,7 @@ public class Car {
         this.weight = weight;
     }
 
-    public Car (){
+    public Car() {
 
     }
 
@@ -27,5 +27,15 @@ public class Car {
                 ", titleModel='" + titleModel + '\'' +
                 ", weight=" + weight +
                 '}';
+    }
+
+    public static void main(String[] args) {
+        Car car1 = new Car(Color.RED);
+        Car car2 = new Car(Color.BLACK, 1580);
+        Car car3 = new Car();
+
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println(car3);
     }
 }
