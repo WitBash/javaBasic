09e2aefabb612fd341af039b1e8e77872543b143
WitBash <yr-83p@mail.ru>
2022-06-26 19:07:19 +0300

    added method "setHouse()" to class House

--- src/com/boshko/lesson3/HW3/House.java
+++ src/com/boshko/lesson3/HW3/House.java
@@ -1,18 +1,22 @@
 package com.boshko.lesson3.HW3;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class House {
     private int numberOfFloors;
     private int yearOfConstruction;
     private String title;
 
-    public House(int numberOfFloors, int yearOfConstruction, String title) {
+    public void setHouse(int numberOfFloors, int yearOfConstruction, String title) {
         this.numberOfFloors = numberOfFloors;
         this.yearOfConstruction = yearOfConstruction;
         this.title = title;
     }
 
-    public int ageBuilding(int year){
-        return year - yearOfConstruction;
+    public int ageBuilding() {
+        Calendar calendar = new GregorianCalendar();
+        return calendar.get(Calendar.YEAR) - yearOfConstruction;
     }
 
     @Override
@@ -25,11 +29,17 @@ public class House {
     }
 
     public static void main(String[] args) {
-        House house1 = new House(3,2018,"Romantic");
-        House house2 = new House(8,2019,"New Wave");
+        House house1 = new House();
+        House house2 = new House();
+
+        house1.setHouse(3, 2018, "Romantic");
+        house2.setHouse(8, 2019, "New Wave");
 
         System.out.println(house1);
         System.out.println(house2);
+
+        System.out.println(house1.ageBuilding());
+        System.out.println(house2.ageBuilding());
     }
 
 }
